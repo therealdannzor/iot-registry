71d805453ffa08c6c558b1c236a5535fadb602a6
dkf <dannzor@outlook.com>
2020-08-23 21:41:24 +0100

    update backend and remove unused methods/functions

--- backend/backend.go
+++ backend/backend.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-const NUMBER_OF_DEVICES = 20
+const NUMBER_OF_DEVICES = 100
 const MAX_SERVED_REQUESTS = 10
 
 // APIcall interface contains the only API call the registry in which it initially uses
@@ -15,10 +15,11 @@ type APIcall interface {
 	// The backend enqueues up to 10 in-flight requests to process at a time until it has finished them all.
 	// Available HTTP status codes are:
 	// 	- 200 (OK): all the (100) devices were successfully registered and returned as a JSON body
-	// 	- 500 (error): the registration process was interrupted/cancelled or have failed in some way;
-	//                 the service proceeds with handling the in-flight requests (up to 10) and then exits
-	// 	- 503 (error): the service is currently operating at full capacity, the user should try again later
+	// 	- 500 (error): the registration process was interrupted/cancelled or have failed in some way
 	Register(wholeID string) error
+
+	// GetList returns the SensorList
+	GetList() [NUMBER_OF_DEVICES]string
 }
 
 // SensorBackend is the backend of the registry
@@ -51,61 +52,12 @@ func (sb SensorBackend) Register(wholeID string) error {
 		return errors.New("device already registered")
 	}
 
-	time.Sleep(50 * time.Millisecond) // simulate system processing time to register a device
+	time.Sleep(75 * time.Millisecond) // simulate system processing time to register a device
 	sb.registry[shortCode] = wholeID  // device added to registry
 
 	return nil
 }
 
-// Enqueue adds a short-format DevEUI to the in-flight queue
-func (sb SensorBackend) Enqueue(shortcodeID string) error {
-	sb.mu.Lock()
-	defer sb.mu.Unlock()
-
-	// throttle the request if operating at max capacity
-	if sb.scLen == 10 {
-		return errors.New("already processing maximum in-flight requests")
-	}
-
-	if shortcodeID == "" || len(shortcodeID) != 5 {
-		return errors.New("incorrect format of device ID, must be of length 5")
-	}
-
-	sb.scheduled[sb.scLen] = shortcodeID
-	sb.scLen++
-	return nil
-}
-
-// Pop removes an ID from the queue and returns it
-func (sb SensorBackend) Pop() (string, error) {
-	sb.mu.Lock()
-	defer sb.mu.Unlock()
-	var emptystring string
-	var ID string
-
-	switch scLen := len(sb.scheduled); scLen {
-	case 0:
-		return emptystring, errors.New("nothing to register")
-	case 1:
-		var empty [10]string
-		ID, sb.scheduled = sb.scheduled[0], empty
-	default:
-		ID, sb.scheduled = sb.scheduled[0], shiftLeft(sb.scheduled)
-	}
-
-	sb.scLen--
-	return ID, nil
-}
-
-func shiftLeft(arr [MAX_SERVED_REQUESTS]string) [MAX_SERVED_REQUESTS]string {
-	lastIndex := MAX_SERVED_REQUESTS - 1
-
-	// move all elements one index left
-	for i := 0; i < lastIndex; i++ {
-		arr[i] = arr[i+1]
-	}
-	// reset last entry to avoid a duplicate of the last element
-	arr[lastIndex] = ""
-
-	return arr
+func (sb SensorBackend) GetList() [NUMBER_OF_DEVICES]string {
+	return sb.SensorList
 }
